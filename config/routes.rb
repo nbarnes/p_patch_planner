9580466f7b4bc824629e8b3dbccd3a4900498aa3
Susan Prestage <susan.prestage@gmail.com>
2014-02-10 13:08:16 -0800

    Set up the home page and give it some structure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PPatchPlanner::Application.routes.draw do
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
