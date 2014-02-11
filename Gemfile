72119e8376a399c9ad739c442347acfb360e9617
NBarnes <nbarnes@gmail.com>
2014-02-10 16:44:56 -0800

    Basic (very basic) weather reporting in place

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+gem 'forecast_io'
+
 group :production do
   # Heroku requirement
   gem 'rails_12factor'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @forecast = get_weather
   end
+
+  def get_weather
+    ForecastIO.api_key = '397e8622e11de93eb5f721bc5cd12c13'
+    ForecastIO.forecast(47.6097, 122.3331).currently.summary
+  end
+
 end
