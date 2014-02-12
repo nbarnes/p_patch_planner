2f59e5f2390d385d8b212026f55a6c2093dd7ae6
Susan Prestage <susan.prestage@gmail.com>
2014-02-11 17:13:16 -0800

    tidy up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ PPatchPlanner::Application.routes.draw do
   resources :news_posts
 
   root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signup',  to: 'users#new', via: 'get'
   match "/auth/twitter/callback" => "sessions#create", via: [:get, :post]
-  match "/signout" => "sessions#destroy", :as => :signout,            via: 'delete'
+  match "/signout" => "sessions#destroy", :as => :signout, via: 'delete'
 
 end
