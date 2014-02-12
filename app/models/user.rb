bb9e28ae2f55c79d52c93b4f9657ff0316130374
NBarnes <nbarnes@gmail.com>
2014-02-11 17:21:06 -0800

    Added email field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   def self.from_omniauth(auth)
     find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
   end
@@ -10,4 +11,5 @@ class User < ActiveRecord::Base
       user.name = auth["info"]["name"]
     end
   end
+
 end
