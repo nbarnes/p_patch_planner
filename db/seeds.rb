20ad5387a257400a4461d713233e89e1c8abc439
NBarnes <nbarnes@gmail.com>
2014-02-17 15:11:45 -0800

    Disqus widget partial in place, not yet inserted into site.  News post seeds updated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,8 @@ User.create(name: 'Deer', uid: '893157', provider: 'twitter')
 User.create(name: 'Elk', uid: '125872', provider: 'twitter')
 
 
-NewsPost.create(title: 'P-Patch open!', body: 'Our new p-patch is open!  Come and see!')
-NewsPost.create(title: 'Coming soon!', body: "Soon we'll have our new p-patch!  Get your shovels ready!")
-NewsPost.create(title: 'Growing Season!', body: 'Spring is almost here!  Come and start preparing!')
+NewsPost.create(title: 'Processus dynamicus qui sequitur mutationem consuetudium lectorum mirum?', body: 'Qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat. In vulputate velit esse molestie consequat vel illum dolore.')
+
+NewsPost.create(title: 'Qui nunc nobis videntur parum, clari fiant sollemnes in.', body: 'Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet. Ii legunt saepius claritas est etiam processus dynamicus qui sequitur mutationem consuetudium lectorum. Esse molestie consequat vel illum dolore eu feugiat nulla facilisis, at vero eros et accumsan. Qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi doming id. Nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex.')
+
+NewsPost.create(title: 'Sit amet consectetuer adipiscing elit sed diam nonummy?', body: 'Ea commodo consequat, duis autem vel eum iriure. Decima eodem modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ad minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl. Dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh.')
